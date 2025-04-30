219d277cb376d31608ec37dca44325d1868c5214
Kimyoonbeom <101162650+Kimyoonbeom@users.noreply.github.com>
2025-04-30 19:36:08 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int solution(int n) {
+
+        String base3 = Integer.toString(n, 3);
+        
+        String reversed = new StringBuilder(base3).reverse().toString();
+        
+        return Integer.parseInt(reversed, 3);
+    }
+}
