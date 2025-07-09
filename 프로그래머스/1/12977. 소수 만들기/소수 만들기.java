6db9efd0cfe12cd0f85204cd7a947b8d08700100
Kimyoonbeom <kimybeom@naver.com>
2025-07-09 12:26:32 +0900

    [level 1] Title: 소수 만들기, Time: 2.66 ms, Memory: 92.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int n = nums.length;
+        // 3개를 고르는 모든 조합 탐색
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    // 소수 판별 함수
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
